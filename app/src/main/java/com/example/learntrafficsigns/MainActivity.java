b654f45d86c05a1ad908ac0fc599740fa9fefb95
nariman <nariman.almanov@outlook.com>
2025-03-26 20:11:11 +0500

    Added navigation menu

--- app/src/main/java/com/example/learntrafficsigns/MainActivity.java
+++ app/src/main/java/com/example/learntrafficsigns/MainActivity.java
@@ -1,24 +1,44 @@
 package com.example.learntrafficsigns;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.material.navigation.NavigationView;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        // Находим RecyclerView
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        // Инициализация DrawerLayout
+        drawerLayout = findViewById(R.id.drawerlayout);
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
 
-        // Указываем, что будет использоваться GridLayoutManager с 2 колонками
+        // Добавляем кнопку меню в Toolbar
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.nav_open, R.string.nav_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Настройка RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
 
         // Данные для категорий
@@ -35,5 +55,29 @@ public class MainActivity extends AppCompatActivity {
         // Устанавливаем адаптер
         SignCategoryAdapter adapter = new SignCategoryAdapter(this, categories);
         recyclerView.setAdapter(adapter);
+    }
+
+    // Обработка выбора пункта меню
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        /*if (id == R.id.nav_home) {
+            // Главное меню
+        } else if (id == R.id.nav_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+        } else if (id == R.id.nav_about) {
+            startActivity(new Intent(this, AboutActivity.class));
+        }*/
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
